aef3a1ed68522137c60de33e1311eb6f1ac1b0f7
iranga <irangacomz@gmail.com>
2021-01-24 22:33:39 +0530

    Spring Initialiser for Spring-Cloud-Config-Server

---
+++ cloud-config-server/src/test/java/com/imm/cloudmicroservices/cloudconfigserver/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imm.cloudmicroservices.cloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
