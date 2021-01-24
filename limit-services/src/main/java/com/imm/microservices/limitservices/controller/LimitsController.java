cc2ec0936a1bd464b944391c4cbbddd780af9dce
iranga <irangacomz@gmail.com>
2021-01-24 22:22:07 +0530

    set the limits values from Configuration file

--- limit-services/src/main/java/com/imm/microservices/limitservices/controller/LimitsController.java
+++ limit-services/src/main/java/com/imm/microservices/limitservices/controller/LimitsController.java
@@ -1,16 +1,24 @@
 package com.imm.microservices.limitservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.imm.microservices.limitservices.bean.Limits;
+import com.imm.microservices.limitservices.configuration.Configuration;
 
 @RestController
 public class LimitsController {
 	
+	//Dependency Injecting
+	@Autowired
+	private Configuration limitsConfiguration;
+	
 	@GetMapping("/limits")
 	public Limits getLimits() {
-		return new Limits(1,100);
+		return new Limits(limitsConfiguration.getMin(),limitsConfiguration.getMax());
+		//get Limits values from Configuration properties/ Application properties
+		//return new Limits(1,100);
 	}
 
 }
