c3e5b163c6c0ea7bef1fa4dca3461e15c20c949f
iranga <irangacomz@gmail.com>
2021-01-25 06:53:37 +0530

    Create Exchange Service Rest Api

---
+++ currency-exchange-service/src/main/java/com/imm/microservices/currencyexchangeservice/controller/CurrencyExchangeService.java
@@ -0,0 +1,34 @@
+package com.imm.microservices.currencyexchangeservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imm.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import com.imm.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
+
+@RestController
+public class CurrencyExchangeService {
+	@Autowired
+	private CurrencyExchangeRepository repository;
+	
+	//Spring Core enviroment to get current server port
+	@Autowired
+	private Environment environment;
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyExchange retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to) {
+		
+		String port =environment.getProperty("local.server.port");
+		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+		currencyExchange.setEnvironment(port);
+		 
+		return currencyExchange;
+	}
+}
+ 
