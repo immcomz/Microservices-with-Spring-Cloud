700974c2760b32e5709012e883ba44df44b69d2b
iranga <irangacomz@gmail.com>
2021-01-25 21:55:19 +0530

    create proxy to make distributed api calls from currency- conversion service to Currency-Exchange Service

---
+++ currency-conversion-service/src/main/java/com/imm/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,20 @@
+package com.imm.microservices.currencyconversionservice.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.imm.microservices.currencyconversionservice.bean.CurrencyConversion;
+
+//set FeignClient to use proxy to currency-exchange-service and url
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	//CurrencyExchange rest api call from currencyConversion controller and
+	//match response entity properties from exchange service to conversion service
+	public CurrencyConversion retrieveExchangeValue(
+			@PathVariable String from,
+			@PathVariable String to);
+
+}
