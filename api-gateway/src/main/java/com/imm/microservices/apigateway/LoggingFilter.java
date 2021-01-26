9870a980c7934d9c9fe9269d0218467b90603223
iranga <irangacomz@gmail.com>
2021-01-26 18:11:19 +0530

    Create a Filter Class to filter request which receive to the API Gateway

---
+++ api-gateway/src/main/java/com/imm/microservices/apigateway/LoggingFilter.java
@@ -0,0 +1,27 @@
+package com.imm.microservices.apigateway;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter{
+
+	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		
+		//get the server request exchange point/Path of the request received
+		logger.info("Path of the request received -> {}", 
+				exchange.getRequest().getPath());
+		
+		return chain.filter(exchange);
+	}
+
+}
